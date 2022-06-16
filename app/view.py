564e3139b83de1d2d6b1e59f283b702396f64a0a
Aleksey Kudryavtcev <st1gii@yahoo.com>
2022-06-16 08:52:38 +0300

    New project

--- app/view.py
+++ app/view.py
@@ -1,5 +1,9 @@
+from flask import render_template
 from app import app
+from flask import render_template
+
 
 @app.route('/')
 def index():
-    return 'BABY GIRL'
+    name = 'Алексей'
+    return render_template('index.html', n = name)
