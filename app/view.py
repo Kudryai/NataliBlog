3152bf8cc11fd694b3b8fdfa57ca3ea0d4c13e90
Aleksey Kudryavtcev <st1gii@yahoo.com>
2022-07-01 16:18:49 +0300

    go ignor

--- app/view.py
+++ app/view.py
@@ -1,10 +1,26 @@
 from flask import render_template
 from app import app
-from flask import render_template
 from models import Post
+from flask import request
+from posts.forms import PostForm
+
 
 @app.route('/')
-def index():
+def index():    
+    q = request.args.get('q')
+    page = request.args.get('page')
+
+    if page and page.isdigit():
+        page = int(page)
+    else:
+        page = 1
+
+    if q:
+        posts = Post.query.filter(Post.title.contains(q) | Post.body.contains(q)) #.all()
+        pages = posts.paginate(page=page, per_page = 5)
+        return render_template('posts/index.html',posts=posts, pages=pages)
+    else:
+        posts = Post.query.order_by(Post.created.desc())
     name = 'Алексей'
     return render_template('index.html', n = name)
 
