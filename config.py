bb8a87ac1ce617d211dba931494b1db2514124db
Aleksey Kudryavtsev <st1gii@yahoo.com>
2022-07-20 21:55:57 +0300

    1

--- config.py
+++ config.py
@@ -1,11 +1,9 @@
-from tkinter import S
-
 
 class Configuration(object):
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:Drygoker1234#@localhost/nataliblog'
-    # SQLALCHEMY_DATABASE_URI = 'postgres://hhfbrywyqgfbtj:2e6ed6ba62f031812521171f9f26d9595ac7b2dfbd39446963ccf6b0704e3a1d@ec2-54-211-255-161.compute-1.amazonaws.com:5432/d4m35ar4vln358'
+    # SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:Drygoker1234#@localhost/nataliblog'
+    SQLALCHEMY_DATABASE_URI = 'postgres://hhfbrywyqgfbtj:2e6ed6ba62f031812521171f9f26d9595ac7b2dfbd39446963ccf6b0704e3a1d@ec2-54-211-255-161.compute-1.amazonaws.com:5432/d4m35ar4vln358'
     SECRET_KEY = 'bueatifull smart screen'
 
 
