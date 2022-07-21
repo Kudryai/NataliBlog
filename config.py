b6924bcb1576c65291882684536ee04f3c1365d6
Aleksey Kudryavtsev <st1gii@yahoo.com>
2022-07-21 20:37:33 +0300

    1

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
+import os
 
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Configuration(object):
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:Drygoker1234#@localhost/nataliblog'
-    SQLALCHEMY_DATABASE_URI = 'postgres://hhfbrywyqgfbtj:2e6ed6ba62f031812521171f9f26d9595ac7b2dfbd39446963ccf6b0704e3a1d@ec2-54-211-255-161.compute-1.amazonaws.com:5432/d4m35ar4vln358'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://') or 'mysql+mysqlconnector://root:Drygoker1234#@localhost/nataliblog'
     SECRET_KEY = 'bueatifull smart screen'
 
 
