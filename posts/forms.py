737d01ce07888ede74f54208516684e49bb3877c
Aleksey Kudryavtsev <st1gii@yahoo.com>
2022-07-24 13:09:52 +0300

    New func

--- posts/forms.py
+++ posts/forms.py
@@ -1,8 +1,11 @@
 from wtforms import Form, StringField, TextAreaField
 from flask_wtf import FlaskForm
 from flask_ckeditor import CKEditorField
+from wtforms.validators import ValidationError, DataRequired
+
 
 class PostForm(Form):
-    title = StringField('Title')
+    title = StringField(label=('Заголовок: '),
+    validators=[DataRequired()])
     body = CKEditorField('Body')
     
